b772b7e5b31b017141ac1d1b0f051de614d4968d
Fatu2024 <fatumab3@gmail.com>
2024-03-14 20:17:07 -0400

    module.js done

---
+++ modules/my-mojule.js
@@ -0,0 +1,17 @@
+const modName = 'mod';
+
+const pet = 'cat';
+
+function hungryCat(){
+    return 'feed the kitty.';
+}
+
+function dirtyCat(){
+    return 'clean the kitty';
+}
+
+function tiredCat(){
+    return 'let kitty rest'
+}
+
+export {modName, pet, hungryCat, dirtyCat, tiredCat };
